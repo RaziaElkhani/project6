eebe6e9a1ebd87625df050310fbf4e78f64e8af8
elkha <elkha@10.0.0.142>
2025-01-30 11:48:43 -0800

    using if statement to compare two variable

---
+++ src/demo2/TestDemo.java
@@ -0,0 +1,13 @@
+package demo2;
+
+public class TestDemo {
+public static void main(String[] args) {
+	int a=10;
+	int b=30;
+	if (a==b) {
+		System.out.println("They are equal");
+	}else {
+		System.out.println("They are not equal");
+	}
+}
+}
