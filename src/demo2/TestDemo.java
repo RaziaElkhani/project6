8204ecdecc0e68745bb7b196fc7c4151057cdc05
elkha <elkha@10.0.0.142>
2025-01-30 21:05:14 -0800

    code had error

--- src/demo2/TestDemo.java
+++ src/demo2/TestDemo.java
@@ -1,17 +1,18 @@
 package demo2;
 
 public class TestDemo {
-public static void main(String[] args) {
-	int a=10;
-	int b=30;
-	if (a==b) {
-		System.out.println("They are equal");
-	}else {
-		System.out.println("They are not equal");
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 30;
+		if (a == b) {
+			System.out.println("They are equal");
+		} else {
+			System.out.println("They are not equal");
+		}
 	}
 
-	public static void getNum(int a ,int b){
-	System.out.println(a+b);	
+	public static void getNum(int a, int b) {
+		System.out.println(a + b);
 	}
-}
+
 }
